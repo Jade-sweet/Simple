f9646446a535b2f7de028da0eed95008d00a02db
jook <1092824310@qq.com>
2020-12-13 23:07:27 +0800

    注册CBV路由

--- api/v1/crawler/urls.py
+++ api/v1/crawler/urls.py
@@ -14,10 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
+from rest_framework.routers import SimpleRouter
 
-from api.v1.crawler.views import add_new, search
+from api.v1.crawler.views import add_new, search, HouseInfoViewSet
+
+router = SimpleRouter()
+router.register('houses', HouseInfoViewSet)
 
 urlpatterns = [
     path('add/', add_new),
     path('search/', search),
 ]
+
+urlpatterns += router.urls
